570c15afe2e58e6cde7f76b173ba8d7dd18edb9c
Davi Lima <davilima6@gmail.com>
2015-08-20 13:11:30 -0300

    Acrescenta testes para instalação do produto e recursos

--- src/d11/hermes/tests/test_setup.py
+++ src/d11/hermes/tests/test_setup.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
-"""Setup tests for this package."""
 from d11.hermes.testing import D11_HERMES_INTEGRATION_TESTING  # noqa
 from plone import api
-
+from plone.browserlayer.utils import registered_layers
 import unittest
+
+JS = [
+    '++resource++d11.hermes/main.js'
+]
+
+CSS = [
+    '++resource++d11.hermes/main.css'
+]
 
 
 class TestSetup(unittest.TestCase):
@@ -26,6 +33,17 @@ class TestSetup(unittest.TestCase):
         from plone.browserlayer import utils
         self.assertIn(ID11HermesLayer, utils.registered_layers())
 
+    def test_jsregistry(self):
+        resource_ids = self.portal.portal_javascripts.getResourceIds()
+        for id in JS:
+            self.assertIn(id, resource_ids, '{0} not installed'.format(id))
+
+    def test_resources_available(self):
+        resources = CSS + JS
+        for id in resources:
+            res = self.portal.restrictedTraverse(id)
+            self.assertTrue(res)
+
 
 class TestUninstall(unittest.TestCase):
 
@@ -39,3 +57,17 @@ class TestUninstall(unittest.TestCase):
     def test_product_uninstalled(self):
         """Test if d11.hermes is cleanly uninstalled."""
         self.assertFalse(self.installer.isProductInstalled('d11.hermes'))
+
+    def test_addon_layer_removed(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertNotIn('ID11HermesLayer', layers)
+
+    def test_jsregistry_removed(self):
+        resource_ids = self.portal.portal_javascripts.getResourceIds()
+        for id in JS:
+            self.assertNotIn(id, resource_ids, '{0} not removed'.format(id))
+
+    def test_cssregistry_removed(self):
+        resource_ids = self.portal.portal_css.getResourceIds()
+        for id in CSS:
+            self.assertNotIn(id, resource_ids, '{0} not removed'.format(id))
